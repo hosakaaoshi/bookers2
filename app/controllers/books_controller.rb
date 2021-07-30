52ea6e459f300be7671bb737dae9b461d451854c
aoshihosaka <aoshisora@icloud.com>
2021-07-30 12:58:26 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = 'successfully created'
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.new
+  end
+
+  def update
+   @book = Book.find(params[:id])
+    if @book.update(@book)
+      flash[:notice] = "successfully update"
+     redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
