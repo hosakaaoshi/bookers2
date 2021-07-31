1bfb32e67c2ab5ee437e4d0098586724da90914f
aoshihosaka <aoshisora@icloud.com>
2021-07-31 14:58:21 +0000

    lnitial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -23,15 +23,6 @@ ActiveRecord::Schema.define(version: 2021_07_28_083403) do
     t.text "body"
   end
 
-  create_table "post_images", force: :cascade do |t|
-    t.text "shop_name"
-    t.string "image_id"
-    t.text "caption"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
