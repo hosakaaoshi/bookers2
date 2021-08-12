885c0cb1cefb6b276d7ca6c927f3295d6c1d6d6b
aoshihosaka <aoshisora@icloud.com>
2021-08-12 13:27:26 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
   end
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
 
   private
