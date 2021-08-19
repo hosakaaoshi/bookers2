36b9a04e8cb5f899902d6648c0e93e578a460d97
aoshihosaka <aoshisora@icloud.com>
2021-08-19 11:54:00 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,10 @@ class UsersController < ApplicationController
   @books = @user.books
   end
   def edit
-   @user = User.find(params[:id])
+  @user = User.find(params[:id])
+  if @user != current_user
+      redirect_to user_path(current_user)
+  end
   end
   def update
     @user = User.find(params[:id])
